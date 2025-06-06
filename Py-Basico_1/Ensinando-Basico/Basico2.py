374caf37a02b21f4784352fccd3b7a8e5f2befb1
JoãoFL <joaofilip444@gmail.com>
2025-06-06 01:23:54 -0300

    Add files via upload

---
+++ Py-Basico_1/Ensinando-Basico/Basico2.py
@@ -0,0 +1,4 @@
+verdura1 = "picanha"
+verdura2 = "sorvete"
+
+print("Eu gosto de comer",verdura1,"e",verdura2)
---
+++ Py-Basico_1/Pratico/At2.py
@@ -0,0 +1,7 @@
+## Atividade 2 ##
+
+n1 = float(input("Primeiro numero: "))
+n2 = float(input("Segundo numero: "))
+n3 = float(input("Terceiro numero: "))
+soma = n1 + n2 + n3
+print("A soma é: ",soma)
